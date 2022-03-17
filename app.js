9d7b0d5660a079e64f1011d4a039f0be13025c22
Adrian HerreraAlgotsson <blazer1458@gmail.com>
2022-03-17 10:12:52 +0100

    fixade bug

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 let calcButton = document.querySelector("#calcButton");
 calcButton.addEventListener("click", Calculate);
 
+let expireDate = document.getElementById("#Utgång");
+expireDate.addEventListener(InsertText);
+
 function Calculate() {
     let investment;
     let years;
@@ -9,8 +12,12 @@ function Calculate() {
     investment = Number(document.form.investering.value);
     years = Number(document.form.år.value);
     
-    if (years > 0 && investment > 0)
-    {
+    if (years > 0 && investment > 0) {
         document.form.resultat.value = Math.pow(increase, years) * investment;
     }
-}   
+}
+
+function InsertText() {
+    
+
+}
